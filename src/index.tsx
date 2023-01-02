eae84b6590bedad18432806ce20906cad697e023
timohaBlr <94timoha@gmail.com>
2023-01-02 18:26:02 +0300

    7-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,62 +1,38 @@
-import React, {useState} from "react";
-
-type samuraiType = {
-    id: string
+type StudentType = {
+    id: number
     name: string
-    status: "online" | "offline"
-}
-type CourseNameType = "HTML"|"JS"|"React"|"Redux"|"HomeWorks"
-type CourseType = {
-    name: CourseNameType
-    mentor: string
-    isDone: boolean
-}
-type TechnologiesType = {
-    [userID: string]: Array<CourseType>
 }
 
-const samuraiID_1 = "64jf-87kg"
-const samuraiID_2 = "90lg-34ks"
-const samuraiID_3 = "12jm-05fd"
+type FriendsType = {
+    [key: string]: Array<string>
+}
 
-export const samurai: Array<samuraiType> = [
-    {id: samuraiID_1, name: "Bob", status: "online"},
-    {id: samuraiID_2, name: "Alex", status: "offline"},
-    {id: samuraiID_3, name: "Ann", status: "offline"},
+export const students: Array<StudentType> = [
+    {id: 1, name: "Bob"},
+    {id: 2, name: "Alex"},
+    {id: 3, name: "Ann"},
+    {id: 4, name: "Charley"},
 ]
 
-export const technologies: TechnologiesType = {
-    [samuraiID_1]: [
-        {name: "HTML", mentor: "Svetlana", isDone: true},
-        {name: "JS", mentor: "Viktor", isDone: true},
-        {name: "React", mentor: "Dmitrij", isDone: false},
-        {name: "Redux", mentor: "Valera", isDone: false},
-        {name: "HomeWorks", mentor: "Ignat", isDone: true},
-    ],
-    [samuraiID_2]: [
-        {name: "HTML", mentor: "Svetlana", isDone: true},
-        {name: "JS", mentor: "Viktor", isDone: true},
-        {name: "React", mentor: "Dmitrij", isDone: true},
-        {name: "Redux", mentor: "Valera", isDone: false},
-        {name: "HomeWorks", mentor: "Ignat", isDone: true},
-    ],
-    [samuraiID_3]: [
-        {name: "HTML", mentor: "Svetlana", isDone: true},
-        {name: "JS", mentor: "Viktor", isDone: true},
-        {name: "React", mentor: "Dmitrij", isDone: false},
-        {name: "Redux", mentor: "Valera", isDone: false},
-        {name: "HomeWorks", mentor: "Ignat", isDone: false},
-    ],
+export const friends: FriendsType = {
+    1: ["Oliver", "Jack", "Oscar",],
+    2: ["Jack", "Lewis", "Thomas",],
+    3: ["William", "Michael", "Lewis",],
+    4: ["Oscar", "James", "William",],
 }
 
-export const updateCourseStatus = (samuraiID: string, name: CourseNameType, isDone: boolean) => {
-    return {...technologies,
-        [samuraiID]: technologies[samuraiID].map(c => c.name === name ? {...c, isDone} : c)
-    }
+const getMutualFriends = (st_1: StudentType, st_2: StudentType,) => {
+    const result: Array<string> = []
+    friends[st_1.id].forEach(f => friends[st_2.id].includes(f)? result.push(f): null)
+    return result
 }
+console.log(getMutualFriends(students[0],students[1]))
 
-// Дан список самураев из инкубатора и структура, хранящая данные о курсах,
-// которые самурай уже прошёл, а так же о тех курсах, которые ему ещё предстоит пройти.
-// Так же дана функция updateCourseStatus,
-// которая позволяет отметить курс как пройденный самураем или снять такую отметку
-// Что надо написать вместо XXX, чтобы функция работала?
+//Дан массив студентов и структура,
+//которая содержит список друзей каждого из студентов.
+// Так же дана функция  getMutualFriends, проверяющая наличие общих друзей
+// у двух выбранных студентов.
+//Функция принимает параметром два объекта типа StudentType
+// и возвращает массив с именами общих друзей,
+//если они есть и пустой массив, если таковых нету.
+// Что надо написать вместо ххх, чтобы функция работала?
