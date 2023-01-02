5bb9b0720725b64e3a879fc9071f51838c12bedd
timohaBlr <94timoha@gmail.com>
2023-01-02 18:31:57 +0300

    7-4

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,61 @@
 import React, {useState} from "react";
 
-type UserType = {
-    id: number
+type samuraiType = {
+    id: string
     name: string
     status: "online" | "offline"
 }
-
-type AddressType = {
-    country: string
-    city: string
-    email: string
+type CourseNameType = "HTML"|"JS"|"React"|"Redux"|"HomeWorks"
+type CourseType = {
+    name: CourseNameType
+    mentor: string
+    isDone: boolean
+}
+type TechnologiesType = {
+    [userID: string]: Array<CourseType>
 }
 
-type AdressesType = {
-    [userID: string]: AddressType
-}
+const samuraiID_1 = "64jf-87kg"
+const samuraiID_2 = "90lg-34ks"
+const samuraiID_3 = "12jm-05fd"
 
-const users: Array<UserType> = [
-    {id: 1, name: "Bob", status: "online"},
-    {id: 2, name: "Alex", status: "offline"},
-    {id: 3, name: "Ann", status: "offline"},
+export const samurai: Array<samuraiType> = [
+    {id: samuraiID_1, name: "Bob", status: "online"},
+    {id: samuraiID_2, name: "Alex", status: "offline"},
+    {id: samuraiID_3, name: "Ann", status: "offline"},
 ]
 
-const addresses: AdressesType = {
-    1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
-    2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
-    3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
-
+export const technologies: TechnologiesType = {
+    [samuraiID_1]: [
+        {name: "HTML", mentor: "Svetlana", isDone: true},
+        {name: "JS", mentor: "Viktor", isDone: true},
+        {name: "React", mentor: "Dmitrij", isDone: false},
+        {name: "Redux", mentor: "Valera", isDone: false},
+        {name: "HomeWorks", mentor: "Ignat", isDone: true},
+    ],
+    [samuraiID_2]: [
+        {name: "HTML", mentor: "Svetlana", isDone: true},
+        {name: "JS", mentor: "Viktor", isDone: true},
+        {name: "React", mentor: "Dmitrij", isDone: true},
+        {name: "Redux", mentor: "Valera", isDone: false},
+        {name: "HomeWorks", mentor: "Ignat", isDone: true},
+    ],
+    [samuraiID_3]: [
+        {name: "HTML", mentor: "Svetlana", isDone: true},
+        {name: "JS", mentor: "Viktor", isDone: true},
+        {name: "React", mentor: "Dmitrij", isDone: false},
+        {name: "Redux", mentor: "Valera", isDone: false},
+        {name: "HomeWorks", mentor: "Ignat", isDone: false},
+    ],
 }
 
-const updateUserAddress = (userID: number, key: string, newValue: string) => {
-    return {...addresses,
-        [userID]: {...addresses[userID], [key]: newValue}
+export const updateCourseStatus = (samuraiID: string, name: CourseNameType, isDone: boolean) => {
+    return {...technologies,
+        [samuraiID]: technologies[samuraiID].map(c => c.name === name ? {...c, isDone} : c)
     }
 }
-console.log(updateUserAddress(2, "city", "Dnepropetrovsk"))
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция updateUserAddress,
-// которая обновляет указанное в параметрах поле в адресе пользователя.
-// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
+// Дан список самураев из инкубатора и структура, хранящая данные о курсах,
+// которые самурай уже прошёл, а так же о тех курсах, которые ему ещё предстоит пройти.
+// Так же дана функция updateCourseStatus,
+// которая позволяет отметить курс как пройденный самураем или снять такую отметку
 // Что надо написать вместо ххх, чтобы функция работала корректно?
