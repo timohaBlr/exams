caba42ef148aac21d95042dd8f12b9c0e10d9074
timohaBlr <94timoha@gmail.com>
2023-01-02 18:30:28 +0300

    7-3

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,42 @@
-type StudentType = {
+import React, {useState} from "react";
+
+type UserType = {
     id: number
     name: string
+    status: "online" | "offline"
 }
 
-type FriendsType = {
-    [key: string]: Array<string>
+type AddressType = {
+    country: string
+    city: string
+    email: string
 }
 
-export const students: Array<StudentType> = [
-    {id: 1, name: "Bob"},
-    {id: 2, name: "Alex"},
-    {id: 3, name: "Ann"},
-    {id: 4, name: "Charley"},
+type AdressesType = {
+    [userID: string]: AddressType
+}
+
+const users: Array<UserType> = [
+    {id: 1, name: "Bob", status: "online"},
+    {id: 2, name: "Alex", status: "offline"},
+    {id: 3, name: "Ann", status: "offline"},
 ]
 
-export const friends: FriendsType = {
-    1: ["Oliver", "Jack", "Oscar",],
-    2: ["Jack", "Lewis", "Thomas",],
-    3: ["William", "Michael", "Lewis",],
-    4: ["Oscar", "James", "William",],
+const addresses: AdressesType = {
+    1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
+    2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
+    3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
+
 }
 
-const getMutualFriends = (st_1: StudentType, st_2: StudentType,) => {
-    const result: Array<string> = []
-    friends[st_1.id].forEach(f => friends[st_2.id].includes(f)? result.push(f): null)
-    return result
+const updateUserAddress = (userID: number, key: string, newValue: string) => {
+    return {...addresses,
+        [userID]: {...addresses[userID], [key]: newValue}
+    }
 }
-console.log(getMutualFriends(students[0],students[1]))
-
-//Дан массив студентов и структура,
-//которая содержит список друзей каждого из студентов.
-// Так же дана функция  getMutualFriends, проверяющая наличие общих друзей
-// у двух выбранных студентов.
-//Функция принимает параметром два объекта типа StudentType
-// и возвращает массив с именами общих друзей,
-//если они есть и пустой массив, если таковых нету.
-// Что надо написать вместо ххх, чтобы функция работала?
+console.log(updateUserAddress(2, "city", "Dnepropetrovsk"))
+// Дан список пользователей и структура, хранящая адреса пользователей.
+// Так же дана функция updateUserAddress,
+// которая обновляет указанное в параметрах поле в адресе пользователя.
+// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
+// Что надо написать вместо ххх, чтобы функция работала корректно?
