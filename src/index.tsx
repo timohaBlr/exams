2be5815bc96bba3f525555bc7d9681af761864ee
timohaBlr <94timoha@gmail.com>
2023-01-03 12:37:42 +0300

    Вторник итоговое -10

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,30 @@
-
-type ActionType = {
-    type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
-    payload: number
+type UserType = {
+    id: number
+    userName: string
+    email: string
+    password: string
 }
 
-export const calculator = (state: number, action: ActionType): number => {
-    switch (action.type) {
-        case "SUM":
-            return state + action.payload
-        case "SUB":
-            return state - action.payload
-        case "DIV":
-            return state / action.payload
-        case "MULT":
-            return state * action.payload
-        case "EXP":
-            return state ** action.payload
-        default:
-            return state
+type ChangeUserPasswordTypeAT = {
+    type: "CHANGE-USER-PASSWORD"
+    payload: {
+        id: number
+        newPassword: string
     }
 }
-const result = calculator(10, {type: "EXP", payload: 0})
-if (!(result - 1)){
-    console.log("IT-INCUBATOR")
-}
 
-//Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
+export const userReducer =
+    (state: UserType[], action: ChangeUserPasswordTypeAT): UserType[] => {
+        switch (action.type) {
+            case "CHANGE-USER-PASSWORD":
+                return state.map(u =>
+                    u.id === action.payload.id
+                        ? {...u, password: action.payload.newPassword}
+                        : u)
+            default:
+                return state
+        }
+    }
+
+//Какой код должен быть написан вместо XXX и YYY в типе //ChangeUserPasswordTypeAT, что бы редьюсер работал?
+//В ответе напишите через пробел: XXX  YYY
