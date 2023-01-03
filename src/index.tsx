d74a0dae83048f0701ccc46c9eb7619ecd7c2901
timohaBlr <94timoha@gmail.com>
2023-01-03 12:28:47 +0300

    Вторник итоговое -4

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,27 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-function PasswordChecker() {
-    const [password, setPassword] = useState<string>("")
-    return (
-        <main>
-            <p>Your password must have more than 8 charters!</p>
-            <input
-                placeholder={"Enter your password"}
-                value={password}
-                onChange={e => setPassword(e.currentTarget.value)}
-                type={"password"}
-            />
-            {password.length < 9 && <p style={{color: "red"}}>The password is too short!</p>}
-        </main>
-    )
+type StudentType = {
+    id: number
+    name: string
 }
 
-ReactDOM.render(
-    <PasswordChecker/>, document.getElementById('root')
-);
+type FriendsType = {
+    [key: string]: Array<string>
+}
 
-// Что надо вставить вместо XXX, чтобы код работал нормально?
+export const students: Array<StudentType> = [
+    {id: 1, name: "Bob"},
+    {id: 2, name: "Alex"},
+    {id: 3, name: "Ann"},
+    {id: 4, name: "Charley"},
+]
+
+export const friends: FriendsType = {
+    1: ["Oliver", "Jack", "Oscar",],
+    2: ["Jack", "Lewis", "Thomas",],
+    3: ["William", "Michael", "Lewis",],
+    4: ["Oscar", "James", "William",],
+}
+console.log(friends[3][1])
+//Дан список студентов и структура,
+//которая содержит список друзей каждого из студентов.
+//Id студента является ключом к массиву его друзей.
+//Какое значение лежит тут:  friends[3][1]?
