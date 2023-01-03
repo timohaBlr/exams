e8af6eda837768fb6ac97e2d60018051aaaa42f1
timohaBlr <94timoha@gmail.com>
2023-01-03 12:36:57 +0300

    Вторник итоговое -9

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 
 type ActionType = {
-    type: "SUM"|"SUB"|"MULT"|"DIV"
+    type: "SUM"|"SUB"|"MULT"|"DIV"|"EXP"
     payload: number
 }
 
@@ -14,11 +14,15 @@ export const calculator = (state: number, action: ActionType): number => {
             return state / action.payload
         case "MULT":
             return state * action.payload
+        case "EXP":
+            return state ** action.payload
         default:
             return state
     }
 }
+const result = calculator(10, {type: "EXP", payload: 0})
+if (!(result - 1)){
+    console.log("IT-INCUBATOR")
+}
 
-const result = calculator(10, {type : "SUB", payload: 5})
-console.log(result)
-//Что надо написать вместо XXX, что бы переменная result содержала значение 5?
+//Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
