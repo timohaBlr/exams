61600f26be777dd51a3e78a12c5396eae3d44818
timohaBlr <94timoha@gmail.com>
2023-01-03 12:36:17 +0300

    Вторник итоговое -8

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,24 @@
-import React, {useState} from "react";
 
-type UserType = {
-    id: number
-    name: string
-    status: "online" | "offline"
+type ActionType = {
+    type: "SUM"|"SUB"|"MULT"|"DIV"
+    payload: number
 }
 
-type AddressType = {
-    country: string
-    city: string
-    email: string
-}
-
-type AdressesType = {
-    [userID: string]: AddressType
-}
-
-const users: Array<UserType> = [
-    {id: 1, name: "Bob", status: "online"},
-    {id: 2, name: "Alex", status: "offline"},
-    {id: 3, name: "Ann", status: "offline"},
-]
-
-const addresses: AdressesType = {
-    1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
-    2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
-    3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
-
-}
-
-const updateUserAddress = (userID: number, key: string, newValue: string) => {
-    return {...addresses,
-        [userID]: {...addresses[userID], [key]: newValue}
+export const calculator = (state: number, action: ActionType): number => {
+    switch (action.type) {
+        case "SUM":
+            return state + action.payload
+        case "SUB":
+            return state - action.payload
+        case "DIV":
+            return state / action.payload
+        case "MULT":
+            return state * action.payload
+        default:
+            return state
     }
 }
-console.log(updateUserAddress(2, "city", "Dnepropetrovsk"))
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция updateUserAddress,
-// которая обновляет указанное в параметрах поле в адресе пользователя.
-// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
-// Что надо написать вместо ххх, чтобы функция работала корректно?
+
+const result = calculator(10, {type : "SUB", payload: 5})
+console.log(result)
+//Что надо написать вместо XXX, что бы переменная result содержала значение 5?
