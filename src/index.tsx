8cf6666a03bdfec94f084220d9a195a32b19e539
timohaBlr <94timoha@gmail.com>
2023-01-03 12:35:33 +0300

    Вторник итоговое -7

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,42 @@
-type StudentType = {
+import React, {useState} from "react";
+
+type UserType = {
     id: number
     name: string
+    status: "online" | "offline"
 }
 
-type FriendsType = {
-    [key: string]: Array<String>
+type AddressType = {
+    country: string
+    city: string
+    email: string
 }
 
-export const students: Array<StudentType> = [
-    {id: 1, name: "Bob"},
-    {id: 2, name: "Alex"},
-    {id: 3, name: "Ann"},
-    {id: 4, name: "Charley"},
+type AdressesType = {
+    [userID: string]: AddressType
+}
+
+const users: Array<UserType> = [
+    {id: 1, name: "Bob", status: "online"},
+    {id: 2, name: "Alex", status: "offline"},
+    {id: 3, name: "Ann", status: "offline"},
 ]
 
-export const friends: FriendsType = {
-    1: ["Oliver", "Jack", "Oscar",],
-    2: ["Jack", "Lewis", "Thomas",],
-    3: ["William", "Michael", "Lewis",],
-    4: ["Oscar", "James", "William",],
+const addresses: AdressesType = {
+    1: {country: "Russia", city: "Moscow", email: "hey@email.com"},
+    2: {country: "Ukraine", city: "Kiev", email: "yo@send.ua"},
+    3: {country: "Belarus", city: "Minsk", email: "wow@gogo.ru"},
+
 }
-console.log(friends[students[0].id][3])
-//Дан список студентов и структура,
-//которая содержит список друзей каждого из студентов.
-//Id студента является ключом к массиву его друзей.
-//Какое значение лежит тут: friends[students[0].id][3]?
 
-
+const updateUserAddress = (userID: number, key: string, newValue: string) => {
+    return {...addresses,
+        [userID]: {...addresses[userID], [key]: newValue}
+    }
+}
+console.log(updateUserAddress(2, "city", "Dnepropetrovsk"))
+// Дан список пользователей и структура, хранящая адреса пользователей.
+// Так же дана функция updateUserAddress,
+// которая обновляет указанное в параметрах поле в адресе пользователя.
+// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
+// Что надо написать вместо ххх, чтобы функция работала корректно?
