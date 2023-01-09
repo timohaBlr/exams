7946c7ece7c4454954d9de6579ce418326db71fc
timohaBlr <94timoha@gmail.com>
2023-01-09 14:58:06 +0300

    Среда - 1 -2

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,12 @@ type StateType = {
     currentPlayPosition: number // milliseconds,
     status: Status
 }
-
 export const playerReducer = (state: StateType, action: any) => {
     switch (action.type) {
-        case 'TRACK-URL-CHANGED':
+        case 'TRACK-STATUS-CHANGED':
             return {
                 ...state,
-                trackUrl: action.url
+                status: action.status
             }
         default:
             return state
@@ -22,4 +21,14 @@ const muteTrackAC = () => ({type: 'TRACK-MUTED'})
 const changeTrackAC = (url: string) => ({type: 'TRACK-URL-CHANGED', url})
 const changeTrackPlayStatusAC = (status: Status) => ({type: 'TRACK-STATUS-CHANGED', status})
 
-//Какой тип должен быть вместо XXX?
+const state: StateType = {
+    status: 'Stopped',
+    currentPlayPosition: 1213,
+    trackUrl: 'https://blabla.com/track01.mp3',
+    volume: 100
+}
+
+const newState = playerReducer(state, changeTrackPlayStatusAC('Paused'))
+console.log(newState.status === 'Paused')
+
+//Напишите вместо XXX правильный вызов правильного AC, чтобы в консоли было true
