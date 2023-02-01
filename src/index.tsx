5d954ed8b9b1e5722501bf48c5ca7c8b3e261166
timohaBlr <94timoha@gmail.com>
2023-02-01 19:35:06 +0300

    Среда - Итоговый -6

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,59 @@
-import React, { useCallback, useState } from 'react'
+import React, {useMemo, useState} from 'react'
 import ReactDOM from 'react-dom'
 
-export const App = () => {
-    const [temp, setTemp] = useState(100)
+type ButtonType = {
+    id: number
+    title: string
+    forAdminOnly: boolean
+}
+const buttons: ButtonType[] = [
+    {id: 1, title: 'delete', forAdminOnly: true},
+    {id: 2, title: 'update', forAdminOnly: true},
+    {id: 3, title: 'create', forAdminOnly: false},
+]
+
+export const App = ({isAdmin}: { isAdmin: boolean }) => {
+
     const [seconds, setSeconds] = useState(0)
 
-    const resetTemp = useCallback(() => setTemp(0), [])
+    const increaseSeconds = () => setSeconds(seconds + 10)
 
-    const incSec = useCallback(() => setSeconds(seconds + 1), [seconds])
+    const correctButtons = useMemo(() => {
+        return buttons.filter(b => isAdmin ? true : !b.forAdminOnly)
+    }, [buttons,isAdmin])
 
     return <>
-        <TempDisplay temp={temp} resetTemp={resetTemp}/>
-        <SecDisplay seconds={seconds} incSec={incSec}/>
-    </>
-}
-const TempDisplay = React.memo((props: any) => {
-    console.log('Render TempDisplay')
-    return (
-        <div style={{marginBottom: '10px'}} onClick={props.reset}>
-            <p>
-                <b>Температура: </b>{props.temp} &#176;
-            </p>
-            <button onClick={props.resetTemp}>Сбросить температуру к 0</button>
-        </div>
-    )
-})
-
-const SecDisplay = React.memo((props: any) => {
-    console.log('Render SecDisplay')
-    return (
+        <ButtonsPanel buttons={correctButtons}/>
         <div>
-            <p><b>Секунды:</b> {props.seconds} c </p>
-            <button style={{marginRight: '20px'}}
-                    onClick={props.incSec}>
-                Увеличить время на 1 секунду
+            <p>
+                <b>Секунды: {seconds}</b>
+            </p>
+            <button onClick={increaseSeconds}>
+                Увеличить на 10 секунд
             </button>
         </div>
+    </>
+}
+
+const ButtonsPanel = React.memo((props: { buttons: Array<ButtonType> }) => {
+    console.log('Render ButtonsPanel')
+    return (
+        <div style={{marginBottom: '15px'}}>
+            <div style={{marginBottom: '15px'}}>
+                <b>Панель с кнопками</b>
+            </div>
+            <div>
+                {props.buttons.map(b => <button key={b.id}>{b.title}</button>)}
+            </div>
+        </div>
     )
 })
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App isAdmin={true}/>, document.getElementById('root'))
 
-// Почему не корректно работает счетчик времени при нажатии на кнопку (срабатывает только 1 раз) ?
-// Найдите в чем причина.
-// Исправленную версию строки напишите в качестве ответа
+// Что нужно написать вместо XXX и YYY,
+// чтобы избавиться от лишнего перерендера компонента ButtonsPanel
+// при нажатии на кнопку "Увеличить на 10 секунд" ?
 
-// Пример ответа: const incSec = () => setSeconds(seconds + 1)
+// Ответ дайте через пробел: 111 222
+
