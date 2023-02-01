2aaa6653181a32e4b6953a060bf2ebe3e06f4837
timohaBlr <94timoha@gmail.com>
2023-02-01 19:19:48 +0300

    Среда - 4 -1

--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,26 @@
-import React, {useMemo, useState} from 'react'
-import ReactDOM from 'react-dom'
+import {combineReducers, createStore} from 'redux'
 
-type ButtonType = {
-    id: number
-    title: string
-    forAdminOnly: boolean
-}
-const buttons: ButtonType[] = [
-    {id: 1, title: 'delete', forAdminOnly: true},
-    {id: 2, title: 'update', forAdminOnly: true},
-    {id: 3, title: 'create', forAdminOnly: false},
-]
-
-export const App = ({isAdmin}: { isAdmin: boolean }) => {
-
-    const [seconds, setSeconds] = useState(0)
-
-    const increaseSeconds = () => setSeconds(seconds + 10)
-
-    const correctButtons = useMemo(() => {
-        return buttons.filter(b => isAdmin ? true : !b.forAdminOnly)
-    }, [buttons, isAdmin])
-
-    return <>
-        <ButtonsPanel buttons={correctButtons}/>
-        <div>
-            <p>
-                <b>Секунды: {seconds}</b>
-            </p>
-            <button onClick={increaseSeconds}>
-                Увеличить на 10 секунд
-            </button>
-        </div>
-    </>
+let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
+const usersReducer = (state = initialState, action: any) => {
+    return state
 }
 
-const ButtonsPanel = React.memo((props: { buttons: Array<ButtonType> }) => {
-    console.log('Render ButtonsPanel')
-    return (
-        <div style={{marginBottom: '15px'}}>
-            <div style={{marginBottom: '15px'}}>
-                <b>Панель с кнопками</b>
-            </div>
-            <div>
-                {props.buttons.map(b => <button key={b.id}>{b.title}</button>)}
-            </div>
-        </div>
-    )
+let authInitialState = {login: 'Margo', settings: {theme: 'dark'}}
+const authReducer = (state = authInitialState, action: any) => {
+    return state
+}
+
+const store = createStore(combineReducers({
+    users: usersReducer,
+    auth: authReducer
+}))
+
+store.subscribe(() => {
+    const login = store.getState().auth.login
+    console.log(login)
 })
 
-ReactDOM.render(<App isAdmin={true}/>, document.getElementById('root'))
+store.dispatch({type: 'ANY'})
+export default store;
 
-// Что нужно написать вместо XXX и YYY,
-// чтобы избавиться от лишнего перерендера компонента ButtonsPanel
-// при нажатии на кнопку "Увеличить на 10 секунд" ?
-
-// Ответ дайте через пробел: 111 222
-
+// Что нужно написать вместо XXX, чтобы в консоли увидеть 'Margo'?
