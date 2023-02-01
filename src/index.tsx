4f019ef7459606f133229031ab84be0c2b0804ae
timohaBlr <94timoha@gmail.com>
2023-02-01 19:21:52 +0300

    Среда - 4 -2

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,25 @@
-import {combineReducers, createStore} from 'redux'
-
-let initialState = {items: [{name: 'Dimych'}, {name: 'Ignat'}]}
-const usersReducer = (state = initialState, action: any) => {
-    return state
+export const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'TRACK-ADDED':
+            return {
+                ...state,
+                [action.trackId]: {
+                    id: action.trackId, likesCount: 0
+                }
+            }
+        default:
+            return state
+    }
 }
 
-let authInitialState = {login: 'Margo', settings: {theme: 'dark'}}
-const authReducer = (state = authInitialState, action: any) => {
-    return state
+const addTrackAC = (trackId: number) => ({type: 'TRACK-ADDED', trackId})
+
+const state = {
+    12: {id: 12, likesCount: 10},
+    14: {id: 14, likesCount: 2},
+    100: {id: 100, likesCount: 0},
 }
+const newState = reducer(state, addTrackAC(300))
+console.log(newState[300].likesCount === 0)
 
-const store = createStore(combineReducers({
-    users: usersReducer,
-    auth: authReducer
-}))
-
-store.subscribe(() => {
-    const login = store.getState().auth.login
-    console.log(login)
-})
-
-store.dispatch({type: 'ANY'})
-export default store;
-
-// Что нужно написать вместо XXX, чтобы в консоли увидеть 'Margo'?
+// Что нужно написать вместо XXX, чтобы в консоли увидеть true?
